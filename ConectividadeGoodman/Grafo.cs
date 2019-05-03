c1516a789cb167d309eef08ec24710b03dae5256
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-05-03 10:48:12 -0300

    Transposta e adicionar aresta no grafo

---
+++ ConectividadeGoodman/Grafo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ConectividadeGoodman
+{
+    class Grafo
+    {
+        public int qtdV; //quantidade de vértices
+        public List<List<int>> listasAdjacencias;
+
+        public Grafo(int qtdV)
+        {
+            this.qtdV = qtdV;
+            var a = new List<int>();
+            this.listasAdjacencias = new List<List<int>>();
+        }
+
+        public Grafo GetTransposta()
+        {
+            Grafo g = new Grafo(qtdV);
+
+            for (int v = 0; v < qtdV; v++)
+            {
+                for (int i = 0; i < listasAdjacencias[v].Count; i++)
+                {
+                    g.listasAdjacencias[i].Add(v);
+                }
+            }
+
+            return g;
+        }
+
+        public void AdicionarAresta(int vo, int vd)
+        {
+            listasAdjacencias[vo].Add(vd);
+        }
+
+
+
+    }
+}

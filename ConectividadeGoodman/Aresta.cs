384b5ea3a9710e3d08669e74a605a92cfd9b1c83
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-05-20 23:10:00 -0300

    Classe Aresta e Vértice

---
+++ ConectividadeGoodman/Aresta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conectividade
+{
+    public class Aresta
+    {
+        private Object element;
+        private Vertice v1, v2;
+        private Boolean direcionada;
+
+        public Aresta(Vertice v1, Vertice v2, object obj, bool direcionada)
+        {
+            this.element = obj;
+            this.v1 = v1;
+            this.v2 = v2;
+            this.direcionada = direcionada;
+        }
+
+    }
+}
---
+++ ConectividadeGoodman/Vertice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conectividade
+{
+    public class Vertice
+    {
+        private object element { get; set; }
+        public Vertice(object obj)
+        {
+            this.element = obj;
+        }
+    }
+}

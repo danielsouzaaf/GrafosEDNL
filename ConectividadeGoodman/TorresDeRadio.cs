7eb8e08f92dc46c173194e8a7365a333c5c5fc4d
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-06-20 02:13:21 -0300

    resolvendo torres de radio ok

--- ConectividadeGoodman/TorresDeRadio.cs
+++ ConectividadeGoodman/TorresDeRadio.cs
@@ -18,55 +18,71 @@ namespace Grafos
                 verticesComGrau.Add((grafo.grau(v), v));
             }
             verticesComGrau.Sort((x, y) => y.Item1.CompareTo(x.Item1));
-            Dictionary<Vertice, string> mapaCores = new Dictionary<Vertice, string>();
-            for (int i = 0; i < verticesComGrau.Count - 1; i++)
+            Dictionary<Vertice, int> mapaCores = new Dictionary<Vertice, int>();
+            int indiceCor = 0;
+            for (int i = 0; i < verticesComGrau.Count; i++)
             {
-                if (mapaCores.ContainsKey(verticesComGrau[i].Item2))
-                    continue;
-                mapaCores.Add(verticesComGrau[i].Item2, "Cor " + i);
-                for (int j = i + 1; j < verticesComGrau.Count - 1; j++)
-                    if (!(grafo.ehAdjacente(verticesComGrau[i].Item2, verticesComGrau[j].Item2)) && !mapaCores.ContainsKey(verticesComGrau[j].Item2))
-                        mapaCores.Add(verticesComGrau[j].Item2, "Cor " + i);
+                if (!mapaCores.ContainsKey(verticesComGrau[i].Item2))
+                {
+                    mapaCores.Add(verticesComGrau[i].Item2, ++indiceCor);
+                    for (int j = i + 1; j < verticesComGrau.Count; j++)
+                        if (!(grafo.ehAdjacente(verticesComGrau[i].Item2, verticesComGrau[j].Item2)) && !mapaCores.ContainsKey(verticesComGrau[j].Item2))
+                        {
+                            int ehadjacente = 0;
+                            foreach (Vertice v in mapaCores.Keys)
+                            {
+                                if (grafo.ehAdjacente(verticesComGrau[j].Item2, v) && mapaCores.GetValueOrDefault(v) == indiceCor)
+                                ehadjacente = 1;
+                            }
+
+                            if (ehadjacente == 0)
+                            {
+                                mapaCores.Add(verticesComGrau[j].Item2, indiceCor);
+                            }
+
+                        }
+                            
+                }
+
             }
 
-            foreach (KeyValuePair<Vertice, string>kvp in mapaCores)
+            foreach (KeyValuePair<Vertice, int>kvp in mapaCores)
             {
-                Console.WriteLine($"Vértice {kvp.Key} - {kvp.Value}");
+                Console.WriteLine($"Vértice {kvp.Key.element} - Cor: {kvp.Value}");
             }
-
 
         }
 
         public TorresDeRadio()
         {
             grafo = new Grafo();
-            grafo.inserirVertice("B");
-            grafo.inserirVertice("A");
-            grafo.inserirVertice("U");
-            grafo.inserirVertice("P");
-            grafo.inserirVertice("Bo");
-            grafo.inserirVertice("Pe");
-            grafo.inserirVertice("C");
-            grafo.inserirVertice("Co");
-            grafo.inserirVertice("E");
-            grafo.inserirVertice("V");
-            grafo.inserirVertice("G");
-            grafo.inserirVertice("S");
-            grafo.inserirVertice("Gf");
+            grafo.inserirVertice("Brasil");
+            grafo.inserirVertice("Argentina");
+            grafo.inserirVertice("Uruguai");
+            grafo.inserirVertice("Paraguai");
+            grafo.inserirVertice("Bolívia");
+            grafo.inserirVertice("Peru");
+            grafo.inserirVertice("Chile");
+            grafo.inserirVertice("Colômbia");
+            grafo.inserirVertice("Equador");
+            grafo.inserirVertice("Venezuela");
+            grafo.inserirVertice("Guiana");
+            grafo.inserirVertice("Suriname");
+            grafo.inserirVertice("Guiana Francesa");
 
-            Vertice Brasil = new Vertice("B");
-            Vertice Argentina = new Vertice("A");
-            Vertice Uruguai = new Vertice("U");
-            Vertice Paraguai = new Vertice("P");
-            Vertice Bolivia = new Vertice("Bo");
-            Vertice Peru = new Vertice("Pe");
-            Vertice Chile = new Vertice("C");
-            Vertice Colombia = new Vertice("Co");
-            Vertice Equador = new Vertice("E");
-            Vertice Venezuela = new Vertice("V");
-            Vertice Guiana = new Vertice("G");
-            Vertice Suriname = new Vertice("S");
-            Vertice GuianaFrancesa = new Vertice("Gf");
+            Vertice Brasil = new Vertice("Brasil");
+            Vertice Argentina = new Vertice("Argentina");
+            Vertice Uruguai = new Vertice("Uruguai");
+            Vertice Paraguai = new Vertice("Paraguai");
+            Vertice Bolivia = new Vertice("Bolívia");
+            Vertice Peru = new Vertice("Peru");
+            Vertice Chile = new Vertice("Chile");
+            Vertice Colombia = new Vertice("Colômbia");
+            Vertice Equador = new Vertice("Equador");
+            Vertice Venezuela = new Vertice("Venezuela");
+            Vertice Guiana = new Vertice("Guiana");
+            Vertice Suriname = new Vertice("Suriname");
+            Vertice GuianaFrancesa = new Vertice("Guiana Francesa");
 
             vertices = grafo.getVertices();
 
@@ -82,40 +98,40 @@ namespace Grafos
             grafo.inserirAresta(grafo.AcharIndice(Brasil), grafo.AcharIndice(Guiana), 0, false);
             grafo.inserirAresta(grafo.AcharIndice(Brasil), grafo.AcharIndice(Suriname), 0, false);
             grafo.inserirAresta(grafo.AcharIndice(Brasil), grafo.AcharIndice(GuianaFrancesa), 0, false);
-/*
+
             //Argentina e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Argentina), vertices.IndexOf(Uruguai), 0, false);
-            grafo.inserirAresta(vertices.IndexOf(Argentina), vertices.IndexOf(Paraguai), 0, false);
-            grafo.inserirAresta(vertices.IndexOf(Argentina), vertices.IndexOf(Bolivia), 0, false);
-            grafo.inserirAresta(vertices.IndexOf(Argentina), vertices.IndexOf(Chile), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Argentina), grafo.AcharIndice(Uruguai), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Argentina), grafo.AcharIndice(Paraguai), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Argentina), grafo.AcharIndice(Bolivia), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Argentina), grafo.AcharIndice(Chile), 0, false);
 
             //Paraguai e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Paraguai), vertices.IndexOf(Bolivia), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Paraguai), grafo.AcharIndice(Bolivia), 0, false);
 
             //Bolívia e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Bolivia), vertices.IndexOf(Chile), 0, false);
-            grafo.inserirAresta(vertices.IndexOf(Bolivia), vertices.IndexOf(Peru), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Bolivia), grafo.AcharIndice(Chile), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Bolivia), grafo.AcharIndice(Peru), 0, false);
 
             //Colômbia e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Colombia), vertices.IndexOf(Peru), 0, false);
-            grafo.inserirAresta(vertices.IndexOf(Colombia), vertices.IndexOf(Equador), 0, false);
-            grafo.inserirAresta(vertices.IndexOf(Colombia), vertices.IndexOf(Venezuela), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Colombia), grafo.AcharIndice(Peru), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Colombia), grafo.AcharIndice(Equador), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Colombia), grafo.AcharIndice(Venezuela), 0, false);
 
             //Equador e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Equador), vertices.IndexOf(Peru), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Equador), grafo.AcharIndice(Peru), 0, false);
 
             //Venezuela e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Venezuela), vertices.IndexOf(Guiana), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Venezuela), grafo.AcharIndice(Guiana), 0, false);
 
             //Guiana e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Guiana), vertices.IndexOf(Suriname), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Guiana), grafo.AcharIndice(Suriname), 0, false);
 
             //Suriname e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Suriname), vertices.IndexOf(GuianaFrancesa), 0, false);
+            grafo.inserirAresta(grafo.AcharIndice(Suriname), grafo.AcharIndice(GuianaFrancesa), 0, false);
 
             //Peru e suas Fronteiras
-            grafo.inserirAresta(vertices.IndexOf(Peru), vertices.IndexOf(Chile), 0, false);
-            */
+            grafo.inserirAresta(grafo.AcharIndice(Peru), grafo.AcharIndice(Chile), 0, false);
+            
         }
        
     }
